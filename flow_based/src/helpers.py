d186f158c998a6e77d4c2ca284d6d2ab3a1cc0eb
Alexandros Petridis <al1998petridis@gmail.com>
2023-10-04 21:48:06 +0200

    take off the 2 features that are not needed

--- flow_based/src/helpers.py
+++ flow_based/src/helpers.py
@@ -22,10 +22,10 @@ active_flows = {}
 timeout_limit = 5995.716184 + 1000 
 
 class uniFlow:
-    def __init__(self, num_pkts, mean_iat, std_iat, min_iat, max_iat, mean_pkt_len, std_pkt_len, min_pkt_len, max_pkt_len, num_bytes, num_psh_flags, num_rst_flags, num_urg_flags):
+    def __init__(self, num_pkts, std_iat, min_iat, max_iat, mean_pkt_len, std_pkt_len, min_pkt_len, max_pkt_len, num_bytes, num_psh_flags, num_rst_flags):
 
         self.num_pkts = num_pkts 
-        self.mean_iat = mean_iat 
+        # self.mean_iat = mean_iat 
         self.std_iat = std_iat 
         self.min_iat = min_iat
         self.max_iat = max_iat
@@ -36,14 +36,14 @@ class uniFlow:
         self.num_bytes = num_bytes
         self.num_psh_flags = num_psh_flags
         self.num_rst_flags = num_rst_flags
-        self.num_urg_flags = num_urg_flags
+        # self.num_urg_flags = num_urg_flags
 
 def uniFlow2df(uniflow):
     """Transform to pandas df in order to use it for prediction"""
-    df = pd.DataFrame(columns=['num_pkts', 'mean_iat', 'std_iat', 'min_iat', 'max_iat', 'mean_pkt_len', 'num_bytes', 'num_psh_flags', 'num_rst_flags', 'num_urg_flags', 'std_pkt_len', 'min_pkt_len', 'max_pkt_len'])
+    df = pd.DataFrame(columns=['num_pkts', 'std_iat', 'min_iat', 'max_iat', 'mean_pkt_len', 'num_bytes', 'num_psh_flags', 'num_rst_flags', 'std_pkt_len', 'min_pkt_len', 'max_pkt_len'])
 
     df.loc[0,'num_pkts'] = int(uniflow.num_pkts)
-    df.loc[0,'mean_iat'] = float(uniflow.mean_iat)
+    # df.loc[0,'mean_iat'] = float(uniflow.mean_iat)
     df.loc[0,'std_iat'] = float(uniflow.std_iat)
     df.loc[0,'min_iat'] = float(uniflow.min_iat)
     df.loc[0,'max_iat'] = float(uniflow.max_iat)
@@ -51,7 +51,7 @@ def uniFlow2df(uniflow):
     df.loc[0,'num_bytes'] = int(uniflow.num_bytes)
     df.loc[0,'num_psh_flags'] = int(uniflow.num_psh_flags)
     df.loc[0,'num_rst_flags'] = int(uniflow.num_rst_flags)
-    df.loc[0,'num_urg_flags'] = int(uniflow.num_urg_flags)
+    # df.loc[0,'num_urg_flags'] = int(uniflow.num_urg_flags)
     df.loc[0,'std_pkt_len'] = float(uniflow.std_pkt_len)
     df.loc[0,'min_pkt_len'] = float(uniflow.min_pkt_len)
     df.loc[0,'max_pkt_len'] = float(uniflow.max_pkt_len)
@@ -146,18 +146,18 @@ def capture_packets(network_interface, model_name):
                 proto = 'TCP'
                 rst_flag = ( l4.flags & dpkt.tcp.TH_RST ) != 0
                 psh_flag = ( l4.flags & dpkt.tcp.TH_PUSH) != 0
-                urg_flag = ( l4.flags & dpkt.tcp.TH_URG ) != 0
+                # urg_flag = ( l4.flags & dpkt.tcp.TH_URG ) != 0
                 fin_flag = ( l4.flags & dpkt.tcp.TH_FIN ) != 0
             elif isinstance(l4, dpkt.udp.UDP):
                 proto = 'UDP' 
                 rst_flag = False
                 psh_flag = False
-                urg_flag = False
+                # urg_flag = False
                 fin_flag = False
             
             filtered_packet_count += 1
             flow_name = "_".join(str(v) for v in (inet_to_str(l3.src), inet_to_str(l3.dst), l4.sport, l4.dport, proto))
-            flow_data = ((rst_flag, psh_flag, urg_flag, fin_flag), header.getts(), len(eth.data))
+            flow_data = ((rst_flag, psh_flag, fin_flag), header.getts(), len(eth.data))
 
             not_exist, time_taken, is_attack = update_flow(flow_name, flow_data, model_name)
             if not_exist:
@@ -216,7 +216,7 @@ def update_flow(flow_name, flow_data, model_name):
     """This function is responsible for creating or updating each flow with the new packets
         Args:
             flow_name: string like the following ipsrc_ipdst_portsrc_portdst_protocol
-            flow_data: turple that contains ((rst_flag, psh_flag, urg_flag, fin_flag), timestamp, ip_len)
+            flow_data: turple that contains ((rst_flag, psh_flag, fin_flag), timestamp, ip_len)
                 timestamp is a tuple with two elements: the number of seconds since the Epoch, and the amount of microseconds past the current second.
         Return return_val, time_to_analyse_flow, is_attack:
             return_val:
@@ -273,8 +273,8 @@ def update_flow(flow_name, flow_data, model_name):
 def analyze_flow(flow_name, model_name):
     """This function is responsible for analyzing the flow each time a new packet is added
 
-        ['num_pkts', 'mean_iat', 'std_iat', 'min_iat', 'max_iat', 'mean_pkt_len',
-            'num_bytes', 'num_psh_flags', 'num_rst_flags', 'num_urg_flags',
+        ['num_pkts', 'std_iat', 'min_iat', 'max_iat', 'mean_pkt_len',
+            'num_bytes', 'num_psh_flags', 'num_rst_flags',
             'std_pkt_len', 'min_pkt_len', 'max_pkt_len', 'is_attack']
         Returns is_attack:
             0: is not an attack
@@ -288,19 +288,19 @@ def analyze_flow(flow_name, model_name):
         length_list = []
         rst_list = []
         psh_list = []
-        urg_list = []
-        # active_flow[flow_name][i] ==> ((rst_flag, psh_flag, urg_flag, fin_flag), timestamp, ip_len) 
+        # urg_list = []
+        # active_flow[flow_name][i] ==> ((rst_flag, psh_flag, fin_flag), timestamp, ip_len) 
         for i in range(len(active_flows[flow_name])): 
             time_list.append(time_tuple_to_float(active_flows[flow_name][i][1]))
             length_list.append(active_flows[flow_name][i][2])
             rst_list.append(active_flows[flow_name][i][0][0])
             psh_list.append(active_flows[flow_name][i][0][1])
-            urg_list.append(active_flows[flow_name][i][0][2])
+            # urg_list.append(active_flows[flow_name][i][0][2])
 
         time_list.sort(reverse = True) # put times in descending order
         t_diff = abs(np.diff(time_list)) # find the time differences
 
-        mean_iat = sum(t_diff) / (num_pkts - 1)
+        # mean_iat = sum(t_diff) / (num_pkts - 1)
         std_iat = np.std(t_diff) # std dev of IAT
         min_iat = min(t_diff)
         max_iat = max(t_diff)
@@ -310,7 +310,7 @@ def analyze_flow(flow_name, model_name):
 
         num_rst_flags = sum(rst_list)
         num_psh_flags = sum(psh_list)
-        num_urg_flags = sum(urg_list)
+        # num_urg_flags = sum(urg_list)
 
         pkt_len_array = np.array(length_list)
         std_pkt_len = float(np.std(pkt_len_array))
@@ -318,7 +318,7 @@ def analyze_flow(flow_name, model_name):
         max_pkt_len = float(max(pkt_len_array))
 
     else:
-        mean_iat = 0.0
+        # mean_iat = 0.0
         std_iat = 0.0
         min_iat = 0.0
         max_iat = 0.0
@@ -328,15 +328,15 @@ def analyze_flow(flow_name, model_name):
 
         num_psh_flags = active_flows[flow_name][0][0][0]
         num_rst_flags = active_flows[flow_name][0][0][1]
-        num_urg_flags = active_flows[flow_name][0][0][2]
+        # num_urg_flags = active_flows[flow_name][0][0][2]
 
         std_pkt_len = 0.0
         min_pkt_len = active_flows[flow_name][0][2]
         max_pkt_len = active_flows[flow_name][0][2]
 
 
-    uniflow = uniFlow(num_pkts, mean_iat, std_iat, min_iat, max_iat, mean_pkt_len, std_pkt_len,
-                      min_pkt_len, max_pkt_len, num_bytes, num_psh_flags, num_rst_flags, num_urg_flags)
+    uniflow = uniFlow(num_pkts, std_iat, min_iat, max_iat, mean_pkt_len, std_pkt_len,
+                      min_pkt_len, max_pkt_len, num_bytes, num_psh_flags, num_rst_flags)
 
     flow_df = uniFlow2df(uniflow)  
 
